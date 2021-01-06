ee0dd78a6541a666026406132dd732997616892c
idrissiradi <idrissiradi@gmail.com>
2021-01-06 15:43:45 +0100

    Login and Logout System

--- django_project/settings.py
+++ django_project/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'users',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
