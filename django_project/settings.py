4317ed30281cfad5a429841c7df067c6e58db004
Ahmed Radi <46751565+idrissiradi@users.noreply.github.com>
2021-01-10 13:25:08 +0100

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -133,5 +133,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'idrissiradiahmed1@gmail.com'
-EMAIL_HOST_PASSWORD = 'ahmedradi4379'
+EMAIL_HOST_USER = '<your email>@gmail.com'
+EMAIL_HOST_PASSWORD = '<your password>'
